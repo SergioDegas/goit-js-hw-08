27a83db3cc510eb253e4e6eeb065e2238b68facb
SergioDegas <laferces@gmail.com>
2022-09-24 18:24:37 +0300

    Initial commit

---
+++ src/js/01-gallery.js
@@ -0,0 +1,31 @@
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+
+const img = galleryItems
+  .map(function (img) {
+    const { description: alt, original: modalImg, preview: galleryImg } = img;
+    // console.log(alt);
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${modalImg}">
+    <img
+      class="gallery__image"
+      src="${galleryImg}"
+      data-source="${modalImg}"
+      alt="${alt}"
+    />
+ </a>
+</div>`;
+  })
+  .join('');
+
+gallery.insertAdjacentHTML('afterbegin', img);
+console.log(gallery);
+
+let galleryLightbox = new SimpleLightbox('.gallery__link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+console.log(galleryItems);
---
+++ src/js/02-video.js
@@ -0,0 +1,16 @@
+
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  // console.log(localStorage);
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
